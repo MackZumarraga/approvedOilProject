1636597b56b75806d555eb15a2d1a46fa95d056f
Mackinjosh Zumarraga <mackzumarraga@gmail.com>
2022-10-26 21:59:07 -0400

    cleaned up

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const db = require("./config/keys").mongoURI;
 const customers = require("./routes/api/customers");
 const products = require("./routes/api/products");
 const orders = require("./routes/api/orders");
-const Customer = require("./models/Customer");
 const bodyParser = require("body-parser");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
-    .then(() => console.log('Connected to mongo db'))
     .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({
@@ -20,14 +18,14 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello poopie!")
+    res.send("")
 });
 
 app.use("/api/customers", customers)
 app.use("/api/products", products)
 app.use("/api/orders", orders)
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+app.listen(port);
 
