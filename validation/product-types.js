06586e59cf5c69898be7cd7c03cd070789c259c8
Mackinjosh Zumarraga <mackzumarraga@gmail.com>
2022-10-26 20:35:41 -0400

    updated product types

--- validation/product-types.js
+++ validation/product-types.js
@@ -1,7 +1,18 @@
 const types = {
-    0: "meat",
-    1: "vegetable",
-    2: "fruit"
+    0: "produce",
+    1: "snacks",
+    2: "meat/fish",
+    3: "condiments",
+    4: "beverages",
+    5: "toiletries",
+    6: "pets",
+    7: "baking/spices",
+    8: "bread/grains",
+    9: "dairy",
+    10: "frozen foods",
+    11: "canned goods",
+    12: "fruits",
+    13: "home"
 }
 
 module.exports = types;
